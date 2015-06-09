1fd3386884a29119f0cf1d1519b89579d8e9e60d
yuqingwu <yuqingwucn.pl@gmail.com>
2015-06-09 11:04:03 +0200

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,29 @@
+//Player.h
+#pragma once
+//Four macros detect five children together into line direction
+#define HORIZON         0
+#define VERTICAL            1
+#define LEFTBOTTOMTORIGHTTOP    2
+#define LEFTTOPTORIGHTBOTTOM    3
+ 
+#include"ChessBoard.h"
+#include<iostream>
+using namespace std;
+#include<string>
+ 
+class Player
+{
+private:
+    string m_name;
+    char m_chessType;
+    int m_x;
+    int m_y;
+    ChessBoard* m_ptBoard;
+public:
+    Player(string name,char chessType):m_name(name),m_chessType(chessType),m_ptBoard(NULL){}
+    void attachToBoard(ChessBoard* ptBoard){m_ptBoard=ptBoard;}
+    bool isInChessBoard(int x,int y);
+    bool isLine(int x,int y);
+    bool isWin();
+    void setChess();
+};

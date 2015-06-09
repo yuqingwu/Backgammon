a5382ec95c0992f2f7a47f5f9ed69e9319ff5b3f
yuqingwu <yuqingwucn.pl@gmail.com>
2015-06-09 11:03:09 +0200

    Create ChessBoard.cpp

---
+++ ChessBoard.cpp
@@ -0,0 +1,23 @@
+//ChessBoard.cpp
+#include"ChessBoard.h"
+ 
+ChessBoard::ChessBoard()    
+{   
+    for(int i=1;i<ROW-1;i++)
+        for(int j=1;j<COL-1;j++)
+            m_cSquare[i][j]=' ';
+    for(int j=0;j<COL;j++)
+        m_cSquare[0][j]=m_cSquare[ROW-1][j]='-';
+    for(int i=1;i<ROW;i++)
+        m_cSquare[i][0]=m_cSquare[i][COL-1]='|';    
+}
+ 
+void ChessBoard::show()
+{
+    system("cls");  
+    for(int i=0;i<ROW;i++)
+    {   for(int j=0;j<COL;j++)
+            cout<<m_cSquare[i][j]<<' ';//here“ <<' ' ”It is important, in order to make on-screen ROW * COL output square
+        cout<<endl;
+    }
+}
